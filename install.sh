287b125369f57a9332285a100294661d3abb3a5e
amitkulkarni1712 <amitkulkarni@djalexander.in>
2018-02-08 13:10:41 +0530

    Changed install

--- install.sh
+++ install.sh
@@ -23,11 +23,7 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer oracle-java8-set-default -y
 
-Install PhpStorm
-wget http://download-cf.jetbrains.com/webide/PhpStorm-2017.1.4.ta‌​r.gz
-tar -xvf PhpStorm-2017.1.4.ta‌​r.gz
-cd PhpStorm-171.4694.2/bin/
-./phpstorm.sh
+sudo snap install phpstorm --classic
 
 ###### GIT ######
 
@@ -41,14 +37,12 @@ sudo apt-get update
 apt-cache policy docker-ce
 
 sudo apt-get install -y docker-ce
-sudo systemctl status docker
 
 
 
 ##### Docker Composer ####
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
 
 
 
